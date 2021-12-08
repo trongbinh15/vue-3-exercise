a9c8ee6444c5934b4bc901ca47894ecf65291400
Trong Binh <trongbinhnguyen15@gmail.com>
2021-12-08 10:38:22 +0700

    add typescript

--- user-management/src/App.vue
+++ user-management/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
     <v-main>
-      <v-progress-linear :indeterminate="loading" color="blue darken-2"></v-progress-linear>
-      <v-container>
-        <router-view></router-view>
-      </v-container>
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-import { useStore } from 'vuex';
-import { computed } from 'vue';
-const store = useStore();
-const loading = computed(() => store.state.base.loading);
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+}
 </script>
